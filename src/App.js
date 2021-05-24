f9b4fce9448a5382f331755821d9a0a042083792
Shubham <shubham.g@testyantra.in>
2021-05-25 00:35:16 +0530

    Routing Login Logout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Login from "./components/login/Login";
+import Landingpage from "./components/landing-page/Landingpage";
+import Employee from "./components/employee/Employee";
+import AddCompany from "./components/company/AddCompany";
+import Company from "./components/company/Company";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Header} />
+          <Route path="/company" component={Company} />
+          <Route path="/about" component={About} />
+          <Route path="/login" component={Login} />
+          <Route path="/add-emp" component={Employee} />
+          <Route path="/add-company" component={AddCompany} />
+          <Route path="/*" component={Landingpage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
